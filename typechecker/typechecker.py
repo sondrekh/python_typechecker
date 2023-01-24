33c24738b4842e86f7b58bc78c33e4cee82ff86a
shk321 <25792547+sondrekh@users.noreply.github.com>
2023-01-24 11:52:18 +0100

    input and output type for decorator

--- typechecker/typechecker.py
+++ typechecker/typechecker.py
@@ -1,6 +1,6 @@
-def typecheck(func):
+def typecheck(func: callable) -> None:
     """Decorator used to enforce type-hints."""
-    def decorated_function(*args, **kwargs):
+    def decorated_function(*args, **kwargs) -> callable:
         Typecheck(func, *args, **kwargs)       
         result = func(*args, **kwargs)
         return result
