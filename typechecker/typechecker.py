905e18ea2168902c886c93698bbdb33be8cf0cf2
shk321 <25792547+sondrekh@users.noreply.github.com>
2023-01-24 11:11:24 +0100

    functional for single arg

--- typechecker/typechecker.py
+++ typechecker/typechecker.py
@@ -1,15 +1,34 @@
 def typecheck(func):
-    def wrapper(*args, **kwargs):
-        for kwarg in kwargs:
-            annotation = func.__annotations__[kwarg]
-            input_type = type(kwargs[kwarg])
+    def decorated_function(*args, **kwargs):
+        annotations = func.__annotations__
 
-            if annotation == input_type:
-                continue
-
-            elif annotation != input_type:
-                raise TypeError("Check function/method input-type(s)")
-            
+        check_kwargs(kwargs, annotations)
+        
+        if len(args) != 1:
+            handle_args(args, annotations)
+       
         result = func(*args, **kwargs)
         return result
-    return wrapper
+
+    return decorated_function
+
+
+
+def check_kwargs(kwargs, annotations):
+    """Typecheck for each provided kwarg."""
+    for kwarg in kwargs:
+        check_type(annotations[kwarg], type(kwargs[kwarg]))
+
+
+def handle_args(args, annotations):
+    i = 1 # First argument in "args" is "self". First input arg is at index 1.
+    for annotation in annotations:
+        if annotation == "return": # Return-value-annotation, not input. 
+            break
+
+        check_type(type(args[i]), annotations[annotation])
+
+
+def check_type(type_input, type_expected): 
+    if type_input != type_expected:
+        raise TypeError("Check function/method input-type(s)")
